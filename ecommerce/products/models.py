97283d0fa9a504d5de2c95571ccf232c9cab4915
jalala-pv <pvjalala@gmail.com>
2024-09-14 12:08:03 +0530

    product_model

--- ecommerce/products/models.py
+++ ecommerce/products/models.py
@@ -4,8 +4,15 @@ from django.db import models
 class Product(models.Model):
     LIVE=1
     DELETE=0
+    DELETE_CHOICES=((LIVE,'live'),(DELETE,'delete'))
     title=models.CharField(max_length=200)
     price=models.FloatField()
     description=models.TextField()
     image=models.ImageField()
     priority=models.IntegerField(default=0)
+    delete_status=models.IntegerField(choices=DELETE_CHOICES,default=LIVE)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+    def str(self)->str:
+        return self.title
