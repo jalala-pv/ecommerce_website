067ff405eebd44bc2c0de91e494b1d906043238e
jalala-pv <pvjalala@gmail.com>
2024-09-16 11:04:38 +0530

    customermodel

--- ecommerce/customers/models.py
+++ ecommerce/customers/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+class Customer(models.Model):
+    LIVE=1
+    DELETE=0
+    DELETE_CHOICES=((LIVE,'live'),(DELETE,'delete'))
+    name=models.CharField(max_length=200)
+    address=models.TextField()
+    #User model is attached to user field in customer
+    user=models.OneToOneField(User,on_delete=models.CASCADE,related_name="customer profile",)
+    phone=models.CharField(max_length=200)
+    delete_status=models.IntegerField(choices=DELETE_CHOICES,default=LIVE)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
 
-# Create your models here.
+    def str(self)->str:
+        return self.name
