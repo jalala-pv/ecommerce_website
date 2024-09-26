aeefe7b9d9b284112f0bcb16d2f6d99182d9b610
jalala-pv <pvjalala@gmail.com>
2024-09-26 21:27:09 +0530

    cart.html

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -23,6 +23,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('products.urls')),
-    path('', include('customers.urls')),
+    path('customer/', include('customers.urls')),
+    path('orders/', include('orders.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)  
---
+++ ecommerce/orders/urls.py
@@ -0,0 +1,11 @@
+
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    
+    path('cart', views.show_cart,name='show_cart')
+    
+
+]
