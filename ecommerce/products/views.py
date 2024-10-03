947dbc6599997ec069276f160530b1011a949635
jalala-pv <pvjalala@gmail.com>
2024-10-03 22:21:53 +0530

    product

--- ecommerce/products/views.py
+++ ecommerce/products/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 def index(request):
     return render(request,'index.html')
 def list_products(request):
+    
     return render(request,'products.html')
 def detail_products(request):
     return render(request,'products_detail.html')
