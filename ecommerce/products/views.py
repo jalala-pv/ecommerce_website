95890ee423ab7862416d66c7846c6834b09b7556
jalala-pv <pvjalala@gmail.com>
2024-10-04 06:46:32 +0530

    passedproductmodeldata

--- ecommerce/products/views.py
+++ ecommerce/products/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 
+from . models import Product
+
 # Create your views here.
 def index(request):
     return render(request,'index.html')
 def list_products(request):
-    
-    return render(request,'products.html')
+    product_list=Product.objects.all()
+    context={'products':product_list}
+    return render(request,'products.html',context)
 def detail_products(request):
     return render(request,'products_detail.html')
